9d4d7d7c53a9daa7b10a8a09402065dda0bb7b2a
originu <originu@gmail.com>
2014-10-17 15:23:52 +0900

    jenkins setting

--- peyto-push-admin/src/main/java/peyto/push/admin/Activator.java
+++ peyto-push-admin/src/main/java/peyto/push/admin/Activator.java
@@ -1,5 +1,7 @@
 package peyto.push.admin;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,7 +17,20 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 public class Activator {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run( Activator.class, args );
+
+		// localhost:9000
+		
+		Process p = null; 
+		try {
+			Thread.sleep( 1000 );
+			String[]	params	= { "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe", "localhost:9000" };
+			p = Runtime.getRuntime().exec( params );
+			p.waitFor();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println(p.exitValue());
+		}
 	}
 }
